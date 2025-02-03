6f7987a1bd5eb5a5f912e71791067c97a9b23e44
Yuriymakedonskiy <urkabestyzhev@gmail.com>
2025-02-04 02:14:14 +0500

    pool snakes new img mp3

--- convert.js
+++ convert.js
@@ -1,7 +1,7 @@
 const sharp = require("sharp");
 
-sharp("IMG_6589.jpg")
+sharp("pool_3.jpg")
   .toFormat("webp")
-  .toFile("pool_6.webp")
+  .toFile("pool_3.webp")
   .then(() => console.log("Conversion successful!"))
   .catch(err => console.error("Error:", err));
